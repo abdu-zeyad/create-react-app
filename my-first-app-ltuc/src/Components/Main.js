ebc3232ed14106f1735cc2cb6cdefd4de3200866
abdu-zeyad <abd.d@live.com>
2021-06-06 17:12:51 +0300

    a

--- my-first-app-ltuc/src/Components/Main.js
+++ my-first-app-ltuc/src/Components/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Hornedbeast1 from './Hornedbeast1'
-import Hornedbeast2 from './Hornedbeast2'
+import Hornedbeast from './Hornedbeast'
 
 class Main extends React.Component {
 
@@ -9,8 +8,8 @@ class Main extends React.Component {
       <div>
         <p>NATURE IS BEAUTIFUL</p>
         <img src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752" alt="that is nature" width='100px' height='100px'  />
-        <Hornedbeast1/>
-        <Hornedbeast2/>
+        <Hornedbeast/>
+        <Hornedbeast/>
 
       </div>
 
