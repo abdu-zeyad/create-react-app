2b25eeb903a736836123c1eabe1c61f1bb66b5d5
abdu-zeyad <abd.d@live.com>
2021-06-07 13:26:25 +0300

    edited

--- my-first-app-ltuc/src/App.js
+++ my-first-app-ltuc/src/App.js
@@ -13,7 +13,6 @@ class App extends React.Component {
         <Header />
         <Main />
         <Footer />
-
       </div>
     )
   }
--- my-first-app-ltuc/src/Components/Main.js
+++ my-first-app-ltuc/src/Components/Main.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import Hornedbeast from './Hornedbeast'
+import Hornedbeast from './Hornedbeast';
 
 class Main extends React.Component {
-
   render() {
     return (
+
+
       <div>
-        <p>NATURE IS BEAUTIFUL</p>
-        <img src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752" alt="that is nature" width='100px' height='100px'  />
-        <Hornedbeast/>
-        <Hornedbeast/>
+
+        <Hornedbeast title={'Lion'} alt={'firstbeast'} description={'The lion is a large felid of the genus Panthera native mainly to Africa. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions have a prominent mane.'} url={'https://www.goway.com/media/cache/aa/79/aa79264f49aae4d4b2d77f0abdeb16fc.jpg'} />
+        <Hornedbeast title={'Tiger'} alt={'sexondbeast'} description={'The tiger is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange-brown fur with a lighter underside. It is an apex predator, primarily preying on ungulates such as deer and wild boar'} url={'https://c402277.ssl.cf1.rackcdn.com/photos/18134/images/hero_full/Medium_WW226365.jpg?1574452099'} />
+        <Hornedbeast title={'Cheatah'} alt={'thirdbeast'} description={'The cheetah is a large cat native to Africa and central Iran. It is the fastest land animal, estimated to be capable of running at 80 to 128 km/h with the fastest reliably recorded speeds being 93 and 98 km/h, and as such has several adaptations for speed, including a light build, long thin legs and a long tail.'} url={'https://media.mehrnews.com/d/2019/05/29/4/3141354.jpg'} />
 
       </div>
-
     )
   }
 }
-
 
 export default Main;
